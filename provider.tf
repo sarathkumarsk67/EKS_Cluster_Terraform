551b2c0aa328687ae17d4b8275730e9c665ba0c6
SARATHKUMAR R <sk5831587@gmail.com>
2025-01-30 19:28:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "project-bucket-devops"
+    bucket = "30-01-2025-13.127.167.212"
     key    = "vgs/terraform.tfstate"
-    region = "us-east-1"   
+    region = "ap-south-1"   
   }
 }
 
@@ -18,10 +18,10 @@ provider "aws" {
 }
 
 locals {
-  region = "us-east-1"
+  region = "ap-south-1"
   name = "vgs_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
